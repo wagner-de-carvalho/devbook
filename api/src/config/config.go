f3d8cfb25eb5410eea094781b44013aeccf1e736
wagner <wagncarv@outlook.com>
2022-09-02 17:05:55 -0300

    Configuração banco de dados

---
+++ api/src/config/config.go
@@ -0,0 +1,12 @@
+package config
+
+var (
+	// String de conexão com o banco de dados
+	StringConexaoBanco = ""
+	// Porta onde a API estará rodando
+	Porta = 0
+)
+// Inicializar variáveis de ambiente
+func Carregar() {
+
+}
