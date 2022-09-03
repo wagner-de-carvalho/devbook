db452100de0c7cf0d72284714bc9addb17e9cf8d
wagner <wagncarv@outlook.com>
2022-09-03 15:18:18 -0300

    Excluir usuário por id

--- api/src/controllers/usuarios.go
+++ api/src/controllers/usuarios.go
@@ -140,5 +140,26 @@ func AtualizarUsuario(w http.ResponseWriter, r *http.Request) {
 
 // Exclui um usuário no banco de dados
 func ExcluirUsuario(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Excluindo usuário"))
+	parametros := mux.Vars(r)
+
+	usuarioID, erro := strconv.ParseUint(parametros["usuarioId"], 10, 64)
+	if erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDeUsuarios(db)
+	if erro = repositorio.Excluir(usuarioID) ; erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	respostas.JSON(w, http.StatusNoContent, nil)
 }
--- api/src/repositorios/usuarios.go
+++ api/src/repositorios/usuarios.go
@@ -114,3 +114,18 @@ func (repositorio Usuarios) Atualizar(ID uint64, usuario modelos.Usuario) error 
 	}
 	return nil
 }
+
+// Excluir excluir usuário pelo id
+func (repositorio Usuarios) Excluir(ID uint64) error {
+	statement, erro := repositorio.db.Prepare("delete from usuarios where id = ?")
+	if erro != nil {
+		return erro
+	}
+	defer statement.Close()
+
+	if _, erro = statement.Exec(ID); erro != nil {
+		return erro
+	}
+
+	return nil
+}
