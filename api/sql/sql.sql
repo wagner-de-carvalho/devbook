97bfa68a63b41076088da6874dcf61aa9ed70317
wagner <wagncarv@outlook.com>
2022-09-04 15:32:50 -0300

    Tabela seguidores

--- api/sql/sql.sql
+++ api/sql/sql.sql
@@ -11,3 +11,16 @@ CREATE TABLE usuarios(
     senha varchar(100) not null,
     criadoEm timestamp default current_timestamp(   )
 ) ENGINE=INNODB;
+
+CREATE TABLE seguidores(
+    usuario_id int not null,
+    FOREIGN KEY (usuario_id) 
+    REFERENCES usuarios(id)
+    ON DELETE CASCADE,
+
+    seguidor_id int not null,
+    FOREIGN KEY (seguidor_id)
+    REFERENCES usuarios(id)
+    ON DELETE CASCADE,
+    PRIMARY KEY (usuario_id , seguidor_id)    
+) ENGINE=INNODB;
