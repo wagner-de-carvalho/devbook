80043506b2c6ade0bce9ca5b4968df05d61a6742
wagner <wagncarv@outlook.com>
2022-09-04 15:22:59 -0300

    Validar token de exclusão de usuário

--- api/src/controllers/usuarios.go
+++ api/src/controllers/usuarios.go
@@ -161,6 +161,17 @@ func ExcluirUsuario(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	usuarioIDNoToken, erro := autenticacao.ExtrairUsuarioID(r)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnauthorized, erro)
+		return
+	}
+
+	if usuarioID != usuarioIDNoToken {
+		respostas.Erro(w, http.StatusForbidden, errors.New("não é possível excluir um usuário diferente do seu"))
+		return
+	}
+
 	db, erro := banco.Conectar()
 	if erro != nil {
 		respostas.Erro(w, http.StatusInternalServerError, erro)
