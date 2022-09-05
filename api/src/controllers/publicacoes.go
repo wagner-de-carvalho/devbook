ff09444287ee6c458fea7357ba4fcf2b8fd14cd4
wagner <wagncarv@outlook.com>
2022-09-05 14:36:38 -0300

    Rotas de publicações

---
+++ api/src/controllers/publicacoes.go
@@ -0,0 +1,24 @@
+package controllers
+
+import "net/http"
+
+// CriarPublicacao adiciona uma nova publicação no banco de dados
+func CriarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
+// BuscarPublicacoes busca as publicações do 'feed' do usuário
+func BuscarPublicacoes(w http.ResponseWriter, r *http.Request) {
+
+}
+// BuscarPublicacao busca uma única publicação
+func BuscarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
+// AtualizarPublicacao atualiza uma única publicação
+func AtualizarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
+// ExcluirPublicacao exclui os dados de uma publicação
+func ExcluirPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
