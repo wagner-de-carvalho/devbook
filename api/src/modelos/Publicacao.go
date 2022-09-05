fafac11371fb9a51603736cf110b04fd3491b7da
wagner <wagncarv@outlook.com>
2022-09-05 17:33:07 -0300

    Formatação de campos da publicação

--- api/src/modelos/Publicacao.go
+++ api/src/modelos/Publicacao.go
@@ -1,13 +1,45 @@
 package modelos
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 // Publicacao representa uma publicação feita por um usuario
 type Publicacao struct {
-	ID uint64 `json:"id,omitempty"`
-	Titulo string `json:"titulo,omitempty"`
-	Conteudo string `json:"conteudo,omitempty"`
-	AutorID int64 `json:"autorId,omitempty"`
-	Curtidas int64 `json:"curtidas"`
+	ID       uint64    `json:"id,omitempty"`
+	Titulo   string    `json:"titulo,omitempty"`
+	Conteudo string    `json:"conteudo,omitempty"`
+	AutorID  int64     `json:"autorId,omitempty"`
+	Curtidas int64     `json:"curtidas"`
 	CriadaEm time.Time `json:"criadaEm,omitempty"`
+}
+
+// Preparar valida e formata os campos da publicação recebida
+func (publicacao *Publicacao) Preparar() error {
+	publicacao.formatar()
+	
+	if erro := publicacao.validar(); erro != nil {
+		return erro
+	}
+
+	return nil
+}
+
+func (publicacao *Publicacao) formatar() {
+	publicacao.Titulo = strings.TrimSpace(publicacao.Titulo)
+	publicacao.Conteudo = strings.TrimSpace(publicacao.Conteudo)
+}
+
+func (publicacao *Publicacao) validar() error {
+	if publicacao.Titulo == "" {
+		return errors.New("o título é obrigatório e não pode estar em branco")
+	}
+
+	if publicacao.Conteudo == "" {
+		return errors.New("o conteúdo é obrigatório e não pode estar em branco")
+	}
+
+	return nil
 }
