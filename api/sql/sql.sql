448503b02d4b6eb20e4d709db6de9b7e105a134c
wagner <wagncarv@outlook.com>
2022-09-05 12:19:24 -0300

    Criação tabela publicações

--- api/sql/sql.sql
+++ api/sql/sql.sql
@@ -25,3 +25,15 @@ CREATE TABLE seguidores(
     ON DELETE CASCADE,
     PRIMARY KEY (usuario_id , seguidor_id)    
 ) ENGINE=INNODB;
+
+CREATE TABLE publicacoes(
+    id int auto_increment primary key,
+    titulo varchar(50) not null,
+    conteudo varchar(300) not null,
+    autor_id int not null,
+    FOREIGN KEY (autor_id) 
+    REFERENCES usuarios(id)
+    ON DELETE CASCADE,
+    curtidas int default 0,
+    criadaEm timestamp default current_timestamp()
+) ENGINE=INNODB;
