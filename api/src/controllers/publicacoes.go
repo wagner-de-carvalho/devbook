8f8134ecb266abd71d8aff9caa99f67913356485
wagner <wagncarv@outlook.com>
2022-09-05 15:05:12 -0300

    Criação de publicações

--- api/src/controllers/publicacoes.go
+++ api/src/controllers/publicacoes.go
@@ -1,23 +1,70 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/autenticacao"
+	"api/src/banco"
+	"api/src/modelos"
+	"api/src/repositorios"
+	"api/src/respostas"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
 
 // CriarPublicacao adiciona uma nova publicação no banco de dados
 func CriarPublicacao(w http.ResponseWriter, r *http.Request) {
+	usuarioID, erro := autenticacao.ExtrairUsuarioID(r)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnauthorized, erro)
+		return
+	}
 
+	corpoDaRequisicao, erro := ioutil.ReadAll(r.Body)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnprocessableEntity, erro)
+		return
+	}
+
+	var publicacao modelos.Publicacao
+	if erro = json.Unmarshal(corpoDaRequisicao, &publicacao); erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	publicacao.AutorID = int64(usuarioID)
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDePublicacoes(db)
+	publicacao.ID, erro = repositorio.Criar(publicacao)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	respostas.JSON(w, http.StatusCreated, publicacao)
 }
+
 // BuscarPublicacoes busca as publicações do 'feed' do usuário
 func BuscarPublicacoes(w http.ResponseWriter, r *http.Request) {
 
 }
+
 // BuscarPublicacao busca uma única publicação
 func BuscarPublicacao(w http.ResponseWriter, r *http.Request) {
 
 }
+
 // AtualizarPublicacao atualiza uma única publicação
 func AtualizarPublicacao(w http.ResponseWriter, r *http.Request) {
 
 }
+
 // ExcluirPublicacao exclui os dados de uma publicação
 func ExcluirPublicacao(w http.ResponseWriter, r *http.Request) {
 
