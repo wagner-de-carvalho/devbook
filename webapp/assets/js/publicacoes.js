c753f27efcd3e40818c819a04b3d0e9b94ee9f9b
wagner <wagncarv@outlook.com>
2022-09-19 13:13:25 -0300

    Curtir publicação

--- webapp/assets/js/publicacoes.js
+++ webapp/assets/js/publicacoes.js
@@ -1,5 +1,6 @@
 // $('#nova-publicacao').on('submit', criarPublicacao);
 $('#botao-publicar').on('click', criarPublicacao);
+$('.curtir-publicacao').on('click', curtirPublicacao);
 
 function criarPublicacao(evento) {
     evento.preventDefault();
@@ -15,4 +16,25 @@ function criarPublicacao(evento) {
     }).fail(function () {
         alert("Erro ao criar publicação!");
     })
+}
+
+function curtirPublicacao(evento) {
+    evento.preventDefault();
+    const elementoClicado = $(evento.target)
+    const publicacaoId = elementoClicado.closest('div').data('publicacao-id');
+
+    elementoClicado.prop('disabled', true);
+    $.ajax({
+        url: `/publicacoes/${publicacaoId}/curtir`,
+        method: 'POST',
+    }).done(function () {
+        const contadorDeCurtidas = elementoClicado.next('span');
+        const quantidadeDeCurtidas = parseInt(contadorDeCurtidas.text());
+
+        contadorDeCurtidas.text(quantidadeDeCurtidas + 1);
+    }).fail(function () {
+        alert('Falha ao curtir a publicação!');
+    }).always(function () {
+        elementoClicado.prop('disabled', false);
+    });
 }
--- webapp/src/respostas/respostas.go
+++ webapp/src/respostas/respostas.go
@@ -16,12 +16,15 @@ func JSON(w http.ResponseWriter, statusCode int, dados interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
 
-	if erro := json.NewEncoder(w).Encode(dados); erro != nil {
-		log.Fatal(erro)
+	if dados != nil {
+		if erro := json.NewEncoder(w).Encode(dados); erro != nil {
+			log.Fatal(erro)
+		}
 	}
+
 }
 
-// TratarStatusCodeDeErro trata as requisições com statusCode da faixa 400 
+// TratarStatusCodeDeErro trata as requisições com statusCode da faixa 400
 func TratarStatusCodeDeErro(w http.ResponseWriter, r *http.Response) {
 	var erro ErroAPI
 	json.NewDecoder(r.Body).Decode(&erro)
